3756b6dc158119d959771ac69da9396ad5e256a7
Juan Aguirre <j1927aguirre@gmail.com>
2022-07-15 23:41:08 -0500

    Handle object as a state

---
+++ src/bases/CounterBy.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+interface CounterProps {
+  initialValue?: number;
+}
+
+export const CounterBy = ({ initialValue = 5 }: CounterProps) => {
+
+  const [counterState, setCounterState] = useState({
+    counter: initialValue,
+    clicks: 0,
+  });
+
+  const { counter, clicks } = counterState;
+
+  const handleCounter = (value: number): void => {
+    setCounterState(prev => ({
+      ...prev,
+      counter: prev.counter + value,
+      clicks: prev.clicks + 1
+    }));
+  };
+
+  return (
+    <>
+      <h1>CounterBy: {counter}</h1>
+      <h1>Clicks: {clicks}</h1>
+      <button onClick={() => handleCounter(1)}>+1</button>
+      <button onClick={() => handleCounter(5)}>+5</button>
+    </>
+  )
+}
