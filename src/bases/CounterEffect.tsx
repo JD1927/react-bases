a20058e154d1d27ad3f9ea19c469c751a1463bd8
Juan Aguirre <j1927aguirre@gmail.com>
2022-07-25 14:26:47 -0500

    Adding gsap and playing with animations

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const MAX_COUNT: number = 10;
 
@@ -10,13 +11,19 @@ export const CounterEffect = () => {
   };
 
   useEffect(() => {
-    if(counter < 10) return;
+    if (counter < 10) return;
     console.log('%cMáx value reached', 'color: tomato; background-color: black;');
+
+    gsap.to('h1', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
+      gsap.to('h1', { y: 0, duration: 1, ease: 'bounce.out' });
+    });
+
   }, [counter]);
 
   return (
     <>
-      <h1>Counter Effect: {counter}</h1>
+      <h1>Counter Effect</h1>
+      <h1>{counter}</h1>
       <button onClick={handleClick}>+1</button>
     </>
   );
